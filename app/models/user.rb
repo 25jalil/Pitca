f7d7c46215f06dc0d154511aef8ea2c513479555
25jalil <25gaisarov@gmail.com>
2017-07-05 19:42:22 +0300

    user index page is set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   enum role: [:user, :seller, :admin]
-  after_initialize :set_default_role, :if => :new_record?
+  after_initialize :self_default_role, if: :new_record?
 
   def self_default_role
     self.role ||= :user
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@ FactoryGirl.define do
   factory :user do
     email 'example@email.com'
     password 'password'
+
+    trait :admin do
+      role 'admin'
+    end
   end
 end
