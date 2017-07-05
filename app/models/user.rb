98200bc38166d67f12d410823589f9d1b910fe9a
25jalil <25gaisarov@gmail.com>
2017-07-05 19:15:43 +0300

    setup user model for roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:user, :seller, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def self_default_role
+    self.role ||= :user
+  end
+
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
 end
