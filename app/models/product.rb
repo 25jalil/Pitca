5eca6da42ae9f36d96c6b9ac6a24bfe062521580
ilnar4888 <25gaisarov@gmail.com>
2017-07-13 12:45:44 +0300

    add_css

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ApplicationRecord
   has_many :orders, through: :ordered_products
 
   validates :name, presence: true, length: { maximum: 30}
-  validates :description, presence: true, length: { maximum: 200}
+  validates :description, presence: true, length: { maximum: 300}
   validates :price, presence:true, numericality: {only_float: true}
   validates_associated :ordered_products
 end
