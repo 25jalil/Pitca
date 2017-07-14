4d75b2abe5e3c82b03ca04b88cb6cd220a69cd2f
ilnar4888 <25gaisarov@gmail.com>
2017-07-14 18:01:32 +0300

    quantity control

--- app/controllers/ordered_products_controller.rb
+++ app/controllers/ordered_products_controller.rb
@@ -1,14 +1,22 @@
 class OrderedProductsController < ApplicationController
   before_action :set_cart, only: [:create]
+
   def create
     product = Product.find(params[:product_id])
     store = Store.find(params[:store_id])
-    @ordered_product = OrderedProduct.new(product: product, store: store, cart: @cart, user: current_user, quantity: 1)
-    if @ordered_product.save
+    ordered_product = OrderedProduct.find_by(product_id: product.id)
+    if ordered_product
+      ordered_product.quantity += 1
+      ordered_product.save
       redirect_to store
     else
-      redirect_to store
-      falsh[:eroor] = "Notice"
+      product.ordered_products.build(store: store, cart: @cart, user: current_user)
+      if product.save
+        redirect_to store
+      else
+        falsh[:eroor] = "Notice"
+        redirect_to store
+      end
     end
   end
 
