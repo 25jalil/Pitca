864b9340f122ab453f423d89cc8c0fd2f3c65f65
ilnar4888 <25gaisarov@gmail.com>
2018-02-28 10:03:12 +0300

    general calendar

--- app/assets/javascripts/admin/full_calendar.js
+++ app/assets/javascripts/admin/full_calendar.js
@@ -1,9 +1,28 @@
 $(function () {
   $('#admin_calendar').fullCalendar({
 
+    customButtons: {
+      myCustomButton: {
+        text: 'Orders store',
+        click: function() {
+          $('#admin_calendar').fullCalendar('removeEvents');
+          $('#admin_calendar').fullCalendar('addEventSource', '/admin/all_orders.json');
+          $('#admin_calendar').fullCalendar('rerenderEvents' );
+        }
+      },
+      allCustomButton: {
+        text: 'Menus store',
+        click: function() {
+          $('#admin_calendar').fullCalendar('removeEvents');
+          $('#admin_calendar').fullCalendar('addEventSource', '/events.json');
+          $('#admin_calendar').fullCalendar('rerenderEvents' );
+        }
+      }
+    },
+
     header: {
       left: 'title',
-      right: 'prev, next'
+      right: 'allCustomButton, myCustomButton, today, prev, next'
     },
     selectable: true,
     selectHelper: true,
